a40a9865c7a29c023326bfebaa267c76043757ed
Jones Alapat <jonesalapat@Joness-MacBook-Air.local>
2021-11-30 21:06:46 +0530

    first commit

---
+++ src/test/java/com/jones/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jones.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
